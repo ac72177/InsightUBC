0466b22960b8d9f0d9af43fc1108e13733a4f2bd
andreasy <ac72177@yahoo.com>
2021-02-03 18:06:08 -0800

    Incomplete update of add Dataset

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -11,22 +11,72 @@ export default class InsightFacade implements IInsightFacade {
 
     constructor() {
         let myMap = new Map();
+        let currentDatasets = [];
         Log.trace("InsightFacadeImpl::init()");
     }
-
+    /**
+     * Add a dataset to insightUBC. delete this later // todo
+     *
+     * @param id  The id of the dataset being added. Follows the format /^[^_]+$/
+     * @param content  The base64 content of the dataset. This content should be in the form of a serialized zip file.
+     * @param kind  The kind of the dataset
+     *
+     * @return Promise <string[]>
+     *
+     * The promise should fulfill on a successful add, reject for any failures.
+     * The promise should fulfill with a string array,
+     * containing the ids of all currently added datasets upon a successful add.
+     * The promise should reject with an InsightError describing the error.
+     *
+     * An id is invalid if it contains an underscore, or is only whitespace characters.
+     * If id is the same as the id of an already added dataset, the dataset should be rejected and not saved.
+     *
+     * After receiving the dataset, it should be processed into a data structure of
+     * your design. The processed data structure should be persisted to disk; your
+     * system should be able to load this persisted value into memory for answering
+     * queries.
+     *
+     * Ultimately, a dataset must be added or loaded from disk before queries can
+     * be successfully answered.
+     */
     public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
-        let allWhiteSpace: boolean = true;
-        for (let char of id) {
-            if (char === "_") {
-                return Promise.reject("InsightError");
+        return this.promiseToVerifyId(id)
+            .then(() => {
+                return this.promiseToVerifyDataset(content);
+            });
+    }
+
+    /**
+     * Turns verifying an id into a Promise
+     * If there is an error, reject with an Insight Error
+     * @param id
+     */
+    private promiseToVerifyId(id: string): Promise<string[]> {
+        return new Promise<string[]> ((resolve, reject) => {
+            let allWhiteSpace: boolean = true;
+            for (let char of id) {
+                if (char === "_") {
+                    reject("InsightError");
+                }
+                if ((char !== "/t") && (char !== "/n")  && (char !==  " ")) {
+                    allWhiteSpace = false;
+                }
             }
-            if ((char !== "/t") && (char !== "/n")  && (char !==  " ")) {
-                allWhiteSpace = false;
+            if (allWhiteSpace) {
+                reject("InsightError");
+            } else {
+                resolve();
             }
-       }
-        if (allWhiteSpace) {
-           return Promise.reject("InsightError");
-        }
+        });
+    }
+
+    /**
+     * Turns verifying a dataset into a promise, adds the dataset to currentDatasets
+     * If there is an error, reject it with an InsightError, JSZip content will go here
+     * @param content
+     */
+    private promiseToVerifyDataset(content: string): Promise<string[]> {
+        // TODO
         return Promise.reject("Not implemented.");
     }
 
