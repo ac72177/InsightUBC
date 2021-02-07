f88dce54077c10ac04cf00e5bbae83d79b15a2a7
LukeLNH <55564765+LukeLNH@users.noreply.github.com>
2021-02-07 12:09:14 +0700

    Implemented the rest of syntax checks for performQuery. Testing is required.

--- src/controller/QueryObject.ts
+++ src/controller/QueryObject.ts
@@ -7,6 +7,8 @@ const SCOMPARATOR: string[] = ["IS"];
 const Sfield: string[] = ["dept", "id", "instructor", "title", "uuid"];
 const NEG: string[] = ["NOT"];
 
+// jsonConstructor check from https://stackoverflow.com/questions/11182924/how-to-check-if-javascript-object-is-json
+const jsonConstructor = ({}).constructor;
 
 // !!! TODO: Implement method to check syntax and grammar of query
 // query contains WHERE and OPTIONS
@@ -22,10 +24,14 @@ export function syntaxCheck(query: any) {
     return;
 }
 
-// WHERE contains only FILTERS
+// WHERE contains only one FILTER object
 function syntaxCheckFilters(query: any) {
-    for (const key in query) {
+    if (!(query.constructor === jsonConstructor)) { throw new InsightError(); }
+    if (Object.keys(query).length > 1) {throw new InsightError(); }
 
+    if (Object.keys(query).length === 1) {
+
+        const key = Object.keys(query)[0];
         if (LOGIC.includes(key)) {
             syntaxCheckLogic(query[key]);
         } else if (MCOMPARATOR.includes(key)) {
@@ -57,10 +63,24 @@ function syntaxCheckOPTIONS(query: any) {
 
 // LOGIC comparisons value must be array containing only FILTER JSON objs
 function syntaxCheckLogic(queryArr: any) { // query must be an array
-    if (!Array.isArray(queryArr)) { throw new InsightError(); }
+    if (!Array.isArray(queryArr) || queryArr.length <= 0) { throw new InsightError(); }
     for (const i in queryArr) {
+        if (queryArr[i].constructor !== jsonConstructor) {throw new InsightError(); }
         syntaxCheckFilters(queryArr[i]);
     }
+    return;
+}
+
+//  id must be one or more of any character, except underscore.
+// !!! TODO: Ask TA what about all spaces?
+function syntaxCheckValidId(id: string) {
+    if (id.includes("_") || id.length < 1) { throw new InsightError(); }
+    return;
+}
+
+function syntaxCheckValidInputString(str: string) {
+    let parsed = str.split("*");
+    if (parsed.length > 1 || parsed[0].length < 1) { throw new InsightError(); }
     return;
 }
 
@@ -68,6 +88,14 @@ function syntaxCheckLogic(queryArr: any) { // query must be an array
 //      obj key must be Mkey and value must be number
 // Mkey and Skey check that idstring is valid.
 function syntaxCheckMComparator(query: any) { // must be one json obj
+    if (!(query.constructor === jsonConstructor)) { throw new InsightError(); }
+    for (let mkey in query) {
+        let parsed = mkey.split("_");
+        if (parsed.length !== 2) { throw new InsightError(); }
+        syntaxCheckValidId(parsed[0]);
+        if (!Mfield.includes(parsed[1])) { throw new InsightError(); }
+        if (typeof(query[mkey]) !== "number") { throw new InsightError(); }
+    }
     return;
 }
 
@@ -75,21 +103,37 @@ function syntaxCheckMComparator(query: any) { // must be one json obj
 //      obj key must be Skey, value must be (valid)inputString. Check *
 // Mkey and Skey check that idstring is valid.
 function syntaxCheckSComparator(query: any) {
+    if (!(query.constructor === jsonConstructor)) { throw new InsightError(); }
+    for (let skey in query) {
+        let parsed = skey.split("_");
+        if (parsed.length !== 2) { throw new InsightError(); }
+        syntaxCheckValidId(parsed[0]);
+        if (!Sfield.includes(parsed[1])) { throw new InsightError(); }
+        if (typeof query[skey] !== "string") { throw new InsightError(); }
+        syntaxCheckValidInputString(query[skey]);
+    }
     return;
 }
 
 // NEGATION value must be one JSON obj containing a FILTER
 function syntaxCheckNegation(query: any) { // query must be an object
+    if (!(query.constructor === jsonConstructor)) { throw new InsightError(); }
+    syntaxCheckFilters(query);
     return;
 }
 
 // COLUMNS must be array of strings
-function syntaxCheckCols(query: any) { // must be array
+function syntaxCheckCols(queryArr: any) { // must be array
+    if (!Array.isArray(queryArr)) { throw new InsightError(); }
+    for (const str of queryArr) {
+        if (typeof str !== "string") {throw new InsightError(); }
+    }
     return;
 }
 
 // if ORDER, value must be a string
 function syntaxCheckOrder(query: any) {
+    if (typeof query !== "string") { throw new InsightError(); }
     return;
 }
 
