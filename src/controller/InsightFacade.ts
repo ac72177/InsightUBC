0fc1ff8d2f58b6dd46eb5a3d2d55c4cb7fb828f3
Lukea <hl2001@students.cs.ubc.ca>
2021-03-18 08:30:13 +0700

    Removed eslint disable comment

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -280,8 +280,6 @@ export default class InsightFacade implements IInsightFacade {
             // return Promise.resolve(queryObject.testGetResLength());
             return Promise.resolve(true);
         } catch (e) {
-            // eslint-disable-next-line no-console
-            console.log(e.message);
             return Promise.resolve(false);
         }
     }
